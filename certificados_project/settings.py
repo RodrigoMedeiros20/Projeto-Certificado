db9e145e817e22e0dfca54d9aa16cdac1e558edc
Rodrigo Rêgo de Medeiros <estagio.medeiros@jfpb.jus.br>
2025-07-24 10:34:01 -0300

    configurações finais

--- certificados_project/settings.py
+++ certificados_project/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'django-insecure-hw&6dxba19m_wd$2a3g)@d@$=_jfhzx@qyz6tlrd@q(16dyfr@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
